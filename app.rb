50342de65d43c069f0734a10b5c8e238e0fa620e
Grace Do <61570746+grace1501@users.noreply.github.com>
2024-11-22 04:37:56 +0000

    started code - set up main files

--- app.rb
+++ app.rb
@@ -1,9 +1,8 @@
 require "sinatra"
 require "sinatra/reloader"
+require "http"
 
 get("/") do
-  "
-  <h1>Welcome to your Sinatra App!</h1>
-  <p>Define some routes in app.rb</p>
-  "
+  
+  erb(:homepage)
 end
