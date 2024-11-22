0bed8e537a58d92845ee5f380a64b82666602829
Grace Do <61570746+grace1501@users.noreply.github.com>
2024-11-22 19:44:48 +0000

    random recipe route finished

--- app.rb
+++ app.rb
@@ -8,11 +8,25 @@ get("/") do
 end
 
 get("/recipes") do
+  search_term = params/.fetch("user_input")
+  api_url = "https://www.themealdb.com/api/json/v1/1/search.php?s=#{search_term}"
+  raw_response = HTTP.get(api_url).to_s
+  response_json = JSON.parse(raw_response)
+  @recipes_list = response_json.fetch("meals")
 
   erb(:recipes)
 end
 
 get("/random") do
+  api_url = "https://www.themealdb.com/api/json/v1/1/random.php"
+  raw_response = HTTP.get(api_url)
+  response_string = raw_response.to_s
+  response_json = JSON.parse(response_string)
 
+  @result = response_json.fetch("meals")[0]
+  @meal_name = @result.fetch("strMeal")
+  @meal_instructions = @result.fetch("strInstructions")
+  @meal_photo = @result.fetch("strMealThumb")
+  
   erb(:random)
 end
